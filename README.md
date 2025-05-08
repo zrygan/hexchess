78f5648217096d2bd6cd4aa2eb76389d73b3bd6a
zrygan <zr.gntn@gmail.com>
2025-05-08 03:52:41 -0400

    Initialized gradle

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # hexchess
+
+By: Zhean Ganituen (`zrygan`)
+Started: May 2025
+Last Updated: May 2025
+_Project in-progress_
+
+## About
+
+Glinski's **hexagonal** chess in Java.
---
+++ app/src/main/java/org/hexchess/Helper.java
@@ -0,0 +1,13 @@
+package org.hexchess;
+
+public class Helper {
+    private String name;
+
+    public Helper(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
